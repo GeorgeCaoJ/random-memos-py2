6d19aeb3ffcc258daff451ce55704b5b1ad42ef0
GerogeCaoJ <cjooo092@gmail.com>
2024-02-18 15:06:40 +0800

    change for python2

--- config.py
+++ config.py
@@ -13,3 +13,8 @@ TG_BOT_TOKEN = "Telegram Bot API Token"
 # Telegram Chat ID
 CHAT_ID = "chat id"
 
+
+# Network Proxies(optional)
+PROXY = 'off'
+PROXY_HTTP = "http://127.0.0.1:7890"
+PROXY_HTTPS = "http://127.0.0.1:7890"
--- random_memo.py
+++ random_memo.py
@@ -1,17 +1,12 @@
 import sqlite3
-import requests
+import urllib2 
 from  config import *
 
-
-db = f'{DB_PATH}/memos_prod.db'
+db = '{}/memos_prod.db'.format(DB_PATH)
 
 def get_random_memo(db='', filter=FILTER): 
     conn = sqlite3.connect(db)
     c = conn.cursor()
-
-    # Get the number of rows in the table
-    c.execute('SELECT COUNT(*) FROM memo')
-    count = c.fetchone()[0]
 
     # Select a random row
     q = 'SELECT * FROM memo ORDER BY RANDOM() LIMIT 1;'
@@ -20,8 +15,8 @@ def get_random_memo(db='', filter=FILTER):
         c.execute(q)
         row = c.fetchone()
 
-        # exclude memo cotaining certain characters.
-        if filter not in row[5]:
+        # exclude ARCHIVED memo and memo cotaining certain characters.
+        if row[4] != 'ARCHIVED' and filter not in row[5]:
             msg = row[5]
 
             if len(msg) <= 500:
@@ -29,8 +24,8 @@ def get_random_memo(db='', filter=FILTER):
             else:
                 msg = msg[0:500] + '...'
 
-            link = f'https://{DOMAIN}/m/{row[0]}'
-            msg = msg  + ' ' + link
+            link = '{}/m/{}'.format(DOMAIN, row[7])
+            msg = u'{}\r\n({})'.format(msg, link)
             break
     conn.close()
     return msg
@@ -38,11 +33,19 @@ def get_random_memo(db='', filter=FILTER):
 
 def push_msg():
     msg = get_random_memo(db, filter=FILTER)
-    url = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
-    params = {'chat_id': CHAT_ID, 'text': msg}
-    response = requests.post(url, params=params)
+    url = 'https://api.telegram.org/bot{}/sendMessage'.format(TG_BOT_TOKEN)
+    params = 'chat_id={}&text={}'.format(CHAT_ID, urllib2.quote(msg.encode('utf-8')))
+    # Create a ProxyHandler and install it in an OpenerDirector
+    if PROXY == 'on':
+        proxy_handler = urllib2.ProxyHandler({'http': PROXY_HTTP, 'https': PROXY_HTTPS})
+        opener = urllib2.build_opener(proxy_handler)
+    else:
+        opener = urllib2.build_opener()
 
-    if response.status_code == 200:
+    # Use the opener to send the request
+    response = opener.open(url, params)
+
+    if response.getcode() == 200:
         print('Message sent.')
     else:
         print('Message sending failed.')
